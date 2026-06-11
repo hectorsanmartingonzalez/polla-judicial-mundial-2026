05af50f8a95d9485ec91813cab285bad6c97788f
hectorsanmartingonzalez <hectorsanmartingonzalez@gmail.com>
2026-06-11 19:24:27 -0400

    Update CUOTA value from 10000 to 15000

--- public/js/config.js
+++ public/js/config.js
@@ -5,5 +5,5 @@
 export const API_URL = "https://script.google.com/macros/s/AKfycbz7NACtBd0mBYShGFOKDx-Fgrmco7OS3Av7fL77F5orMzPk8fJGvLlbWxze-_JWc1t0TQ/exec";
 
 export const ADMIN_MAIL = "hectorsanmartingonzalez@gmail.com";
-export const CUOTA = 10000;
+export const CUOTA = 15000;
 export const FECHA_LIMITE_PAGO = "19 de julio";
